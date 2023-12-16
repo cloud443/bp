e6a2a2a1234572c65dc2e9ae67f5ca66d3c0a40a
cloud443 <121417859+cloud443@users.noreply.github.com>
2023-12-17 01:32:56 +0530

    Update BloodPressure.cs

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 
@@ -7,10 +7,10 @@ namespace BPCalculator
     // BP categories
     public enum BPCategory
     {
-        [Display(Name="Low Blood Pressure")] Low,
-        [Display(Name="Ideal Blood Pressure")]  Ideal,
-        [Display(Name="Pre-High Blood Pressure")] PreHigh,
-        [Display(Name ="High Blood Pressure")]  High
+        [Display(Name = "Low Blood Pressure")] Low,
+        [Display(Name = "Ideal Blood Pressure")] Ideal,
+        [Display(Name = "Pre-High Blood Pressure")] PreHigh,
+        [Display(Name = "High Blood Pressure")] High
     };
 
     public class BloodPressure
@@ -31,9 +31,23 @@ namespace BPCalculator
         {
             get
             {
-                // implement as part of project
-                //throw new NotImplementedException("not implemented yet");
-                return new BPCategory();                       // replace this
+                // According to the chart provided
+                if (Systolic >= 140 || Diastolic >= 90)
+                {
+                    return BPCategory.High;
+                }
+                else if (Systolic >= 120 || Diastolic >= 80)
+                {
+                    return BPCategory.PreHigh;
+                }
+                else if (Systolic >= 90 || Diastolic >= 60)
+                {
+                    return BPCategory.Ideal;
+                }
+                else
+                {
+                    return BPCategory.Low;
+                }                      // replace this
             }
         }
     }
